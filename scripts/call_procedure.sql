da1c520c97835938ab5b8c1f037a724c1f66fd22
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-05-25 03:42:10 +0530

    Update call_procedure.sql

--- scripts/call_procedure.sql
+++ scripts/call_procedure.sql
@@ -1,1 +1,10 @@
+/*
+=============================================================
+Execute Procedure to Create Bronze Tables
+=============================================================
+This script simply calls the bronze.create_all_bronze_tables() procedure.
+=============================================================
+*/
+
+CALL bronze.create_all_bronze_tables();
 
