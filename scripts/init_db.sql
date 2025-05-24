2d27ac1f7eccdc33a4cd3707da23551b3b2aceb1
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-05-25 01:55:18 +0530

    Update init_db.sql

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -1,1 +1,18 @@
+DO $$
+BEGIN
+   IF EXISTS (SELECT 1 FROM pg_database WHERE datname = 'datawarehouse') THEN
+      -- Terminate all connections to the database
+      REVOKE CONNECT ON DATABASE datawarehouse FROM public;
+      EXECUTE format('SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = %L;', 'datawarehouse');
+      -- Drop the database
+      EXECUTE 'DROP DATABASE datawarehouse';
+   END IF;
+END$$;
 
+-- Create the 'datawarehouse' database
+CREATE DATABASE datawarehouse;
+
+
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
